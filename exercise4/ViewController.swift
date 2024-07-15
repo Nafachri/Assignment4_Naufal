245c3607650bcf632acb72bca04279d5478ee83e
Nafachri <nafa.create@gmail.com>
2024-07-15 11:28:32 +0700

    Initial Commit

---
+++ exercise4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  exercise4
+//
+//  Created by Naufal Al-Fachri on 15/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
